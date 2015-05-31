3d3deadb7ef3b47d95f1b18aeab0d69c723395cd
Michael Feldstein <msfeldstein@gmail.com>
2015-05-31 13:30:45 -0700

    Constantify the out path

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var OUT_PATH = "dist";
+
 var watchify = require('watchify');
 var browserify = require('browserify');
 var coffeeify = require('gulp-coffeeify');
@@ -17,24 +19,21 @@ var rename = require('gulp-rename');
 var es6transpiler = require('gulp-es6-transpiler');
 var bonjour = require('frameless-connect');
 
-var OUT_PATH = "dist";
-
 gulp.task('default', function() {
   gulp.start('style');
   gulp.start('coffee');
   gulp.start('js');
   gulp.start('images');
   gulp.start('copy-index');
+
   gulp.watch('src/style.scss', ['style']);
   gulp.watch('src/**/*.js', ['js']);
   gulp.watch('src/**/*.coffee', ['coffee']);
   gulp.watch('src/**/*.html', ['copy-index']);
-  // Watch image files
   gulp.watch('src/images/**/*', ['images']);
   watch(OUT_PATH).pipe(connect.reload());
   gulp.start('connect');
 });
-
 
 gulp.task('coffee', function() {
   var coffeeFiles = glob.sync('./src/index.coffee');
@@ -48,11 +47,11 @@ gulp.task('coffee', function() {
     .on('update', function() {
       watcher.bundle()
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./dist'))
+      .pipe(gulp.dest(OUT_PATH))
     })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(OUT_PATH))
 });
 
 gulp.task('js', function() {
@@ -65,24 +64,29 @@ gulp.task('js', function() {
     .on('update', function() {
       watcher.bundle()
       .pipe(source('bundle.js'))
-      .pipe(gulp.dest('./dist'))
+      .pipe(gulp.dest(OUT_PATH))
     })
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest(OUT_PATH))
 });
 
 
 gulp.task('images', function() {
   return gulp.src('src/images/**/*')
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest(OUT_PATH + '/images'));
 });
 
 gulp.task('style', function() {
   return gulp.src('src/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest(OUT_PATH));
 });
+
+gulp.task('copy-index', function() {
+  gulp.src('./src/index.html')
+    .pipe(gulp.dest(OUT_PATH))
+})
 
 gulp.task('connect', function() {
   connect.server({
@@ -93,8 +97,3 @@ gulp.task('connect', function() {
   spawn('open', ['http://localhost:8080']);
   bonjour(8080);
 });
-
-gulp.task('copy-index', function() {
-  gulp.src('./src/index.html')
-    .pipe(gulp.dest('./dist'))
-})
