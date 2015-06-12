e5fe1aa7a525d88e5983f5258b37cda9710a5d4a
Michael Feldstein <msfeldstein@gmail.com>
2015-06-12 00:29:04 -0700

    Add generator

---
+++ bin/generate.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var fs = require('fs');;
+var fsx = require('fs-extra');
+var npm = require('npm');
+
+var projectName = process.argv[2];
+console.log("Generating Project: ", projectName)
+
+var newdir = process.cwd() + "/" + projectName
+fs.mkdirSync(projectName);
+var neededFiles = ["src", "gulpfile.js", "package.json", "README.md"]
+neededFiles.forEach(function(filename) {
+  var file = __dirname + '/../' + filename;
+  fsx.copySync(file, newdir + "/" + filename);
+});
