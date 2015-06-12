cb88051e8ea15aa501247a69428ea1dc5f99d84e
Michael Feldstein <msfeldstein@gmail.com>
2015-06-12 00:48:09 -0700

    rename the package.json project name

--- bin/generate.js
+++ bin/generate.js
@@ -2,10 +2,12 @@
 var fs = require('fs');;
 var fsx = require('fs-extra');
 var npm = require('npm');
+var child_process = require('child_process');
 
 var projectName = process.argv[2];
 console.log("Generating Project: ", projectName)
 
+// Create the directory and copy all the files over
 var newdir = process.cwd() + "/" + projectName
 fs.mkdirSync(projectName);
 var neededFiles = ["src", "gulpfile.js", "package.json", "README.md"]
@@ -14,7 +16,15 @@ neededFiles.forEach(function(filename) {
   fsx.copySync(file, newdir + "/" + filename);
 });
 
+// Move into the project directory
 process.chdir(projectName);
+
+// change the name of the npm project to the name of your project
+var packageJson = fs.readFileSync("package.json").toString();
+packageJson = packageJson.replace("PrototypeBase", projectName);
+fs.writeFileSync("package.json", packageJson, 'utf8');
+
+// run `npm install`
 npm.load(function() {
   npm.commands.install();
   npm.on("log", function(message) {
